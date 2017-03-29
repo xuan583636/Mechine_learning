8e9787f74738aea393adbf9dfa7266fbd658f967
胡文杰 <hwj950504@163.com>
2017-03-29 22:12:59 +0800

    10折交差验证完成

--- error_rate.py
+++ error_rate.py
@@ -9,22 +9,46 @@ from logarithmic_regression import logarithmic_regression
 def main():
     data = readxls.excel_table_byrow_and_col("/Users/JJjie/Desktop/www/Mechine_Learning/dataset/UCI-iris数据集.xlsx", "Sheet1", (6,0),(12,100))
     d = mat(data)
+    k = 100  # 样本总个数
 
     # 10折交差验证
+    print "10折交差验证法"
     err0 = 0
+    # 样本分割
     for tn in range(0, 9):
         reset = d[:, (tn*5):((tn+1)*5)]
-        set = mat(zeros((6,0)))
+        reset = concatenate((reset, d[:, (50+tn*5):(50+(tn+1)*5)]), axis=1)
+        set = 0
         if tn == 0:
             set = d[:, ((tn+1)*5):50]
+            set = concatenate((set, d[:, (50+(tn+1)*5):100]), axis=1)
         else:
             set = d[:, 0:(tn*5)]
-            set = hstack((set, d[:, ((tn+1)*5):50])) # 行合并
+            set = concatenate((set, d[:, ((tn + 1) * 5):50]), axis=1)
+            set = concatenate((set, d[:, 0:(50+tn*5)]), axis=1)
+            set = concatenate((set, d[:, (50+(tn + 1) * 5):100]), axis=1)
 
-        b = logarithmic_regression(set, 45, 4)
+        xdata = set[0:5].T
+        ydata = set[5].T
+        ydata -= 1
+        b = logarithmic_regression(xdata, ydata, 4)
+        print "B的值为：", b
+        b = b.T
+        x = reset[0:5]
+        y = mat(reset[5]).T
+        y = y - 1
+        tmp = 1 / (1 + exp(-b * x))
+        tmp = (tmp >= 0.5)
+        tmp = tmp.A[0]
+        for i in range(len(y)):
+            if tmp[i] != y[i]:
+                err0 += 1
+
+    print "预测样本一共%d个，预测错误%d个" % (10*10, err0)
 
 
     # 留出法
+    print "\n留出法"
     err1 = 0
     set = d[:, 0:40] # 数组切片
     reset = d[:, 40:50]
@@ -35,7 +59,7 @@ def main():
     ydata = mat(set[5]).T
     ydata = ydata - 1
     b = logarithmic_regression(xdata, ydata, 4)
-    print b
+    print "B的值为：", b
     b = b.T
     x = reset[0:5]
     y = mat(reset[5]).T
@@ -46,7 +70,7 @@ def main():
     for i in range(20):
         if tmp[i] != y[i]:
             err1 += 1
-    print err1
+    print "预测样本一共%d个，预测错误%d个" % (20,err1)
 
 if __name__ == '__main__':
     main()
