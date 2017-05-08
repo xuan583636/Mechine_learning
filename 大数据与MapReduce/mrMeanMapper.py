7e401e9acb0821d529f080fa9d5a3c7fb515dfeb
胡文杰 <hwj950504@163.com>
2017-05-08 21:28:44 +0800

    添加yield的注解

--- 大数据与MapReduce/mrMeanMapper.py
+++ 大数据与MapReduce/mrMeanMapper.py
@@ -20,7 +20,16 @@ import numpy as np
 
 def read_input(file):
     for line in file:
+        # http://www.cnblogs.com/tqsummer/archive/2010/12/27/1917927.html
+        """
+        包含yield语句的函数会被特地编译成生成器。
+        当函数被调用时，他们返回一个生成器对象，这个对象支持迭代器接口。
+        函数也许会有个return语句，但它的作用是用来yield产生值的。
+        不像一般的函数会生成值后退出，生成器函数在生成值后会自动挂起并暂停他们的执行和状态，
+        他的本地变量将保存状态信息，这些信息在函数恢复时将再度有效
+        """
         yield line.rstrip()         #取消尾部的字节
+
 
 input = read_input(sys.stdin)
 input = [float(line) for line in input]
