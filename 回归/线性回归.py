19ec6e5f0216dbbead6984e25345048c4d098d96
胡文杰 <hwj950504@163.com>
2017-05-21 13:36:49 +0800

    示例：预测乐高玩具套装的价格

--- 回归/线性回归.py
+++ 回归/线性回归.py
@@ -59,6 +59,9 @@ def lwlrTest(testArr, xArr, yArr, k=1.0):
         yHat[i] = lwlr(testArr[i], xArr, yArr, k)
     return yHat
 
+def rssError(yArr,yHatArr): #yArr and yHatArr both need to be arrays
+    return ((yArr-yHatArr)**2).sum()
+
 # 处理特征比样本多的情况
 # 岭回归
 def ridgeRegres(xMat, yMat, lam=0.2):
@@ -124,6 +127,41 @@ def drawpic(xMat, yMat, yHat):
     ax.plot(xSort[:, 1], yHat[strInd][:,0,:])
     plt.show()
 
+# 示例：预测乐高玩具套装的价格
+from time import sleep
+import json
+import urllib2
+def searchForSet(retX, retY, setNum, yr, numPce, origPrc):
+    sleep(10)
+    myAPIstr = 'get from code.google.com'
+    searchURL = "https://www.googleapis.com/shopping/search/v1/public/products?key=%s&country=US&q=lego+%d&alt=json" % (myAPIstr, setNum)
+    pg = urllib2.urlopen(searchURL)
+    retDict = json.loads(pg.read())
+    for i in range(len(retDict['items'])):
+        try:
+            currItem = retDict['items'][i]
+            if currItem['product']['condition'] == 'new':
+                newFlag = 1
+            else:
+                newFlag = 0
+            listOfInv = currItem['product']['inventories']
+            for item in listOfInv:
+                sellingPrice = item['price']
+                if sellingPrice > origPrc * 0.5:
+                    print "%d\t%d\t%d\t%f\t%f" % (yr, numPce, newFlag, origPrc, sellingPrice)
+                    retX.append([yr, numPce, newFlag, origPrc])
+                    retY.append(sellingPrice)
+        except:
+            print 'problem with item %d' % i
+
+def setDataCollect(retX, retY):
+    searchForSet(retX, retY, 8288, 2006, 800, 49.99)
+    searchForSet(retX, retY, 10030, 2002, 3096, 269.99)
+    searchForSet(retX, retY, 10179, 2007, 5195, 499.99)
+    searchForSet(retX, retY, 10181, 2007, 3428, 199.99)
+    searchForSet(retX, retY, 10189, 2008, 5922, 299.99)
+    searchForSet(retX, retY, 10196, 2009, 3263, 249.99)
+
 
 if __name__ == '__main__':
     xArr, yArr = loadDataset("/Users/JJjie/Desktop/Projects/dataset/MLiA/ex0.txt")
@@ -140,11 +178,17 @@ if __name__ == '__main__':
 
     abx, aby = loadDataset("/Users/JJjie/Desktop/Projects/dataset/MLiA/abalone.txt")
     ridgeWeights = ridgeTest(abx, aby)
-    import matplotlib.pyplot as plt
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    print np.shape(ridgeWeights)
-    ax.plot(ridgeWeights)
-    plt.show()
+    # import matplotlib.pyplot as plt
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111)
+    # print np.shape(ridgeWeights)
+    # ax.plot(ridgeWeights)
+    # plt.show()
 
+    lgx = []
+    lgy = []
+    setDataCollect(lgx, lgy)
+    print lgx
+    print
+    print lgy
 
